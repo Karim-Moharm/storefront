b9fac6b2ebbc05d870ba3d865516b93b34b04cc0
karim-moharm <karimmoharm222@gmail.com>
2025-12-08 13:15:13 +0200

    question about the Protected ondelete

--- store/models.py
+++ store/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     invertory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    # PROTECT -> Prevents deleting the parent (Collection) if children (Product) exist .
+    Collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
 class Customer(models.model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
@@ -34,6 +39,15 @@ class Order(models.Model):
         choices=[("P", "Pending"), ("C", "Complete"), ("F", "Failed")],
         default="P",
     )
+    # the customer can have multiple orders, but the order belongs to one customer
+    # so it's one to many rel
+    # so the order table will have the customer foreign key
+    # PROECT -> because you don't want to deleted an order from the data base even if the customr is deleted
+    # but it will prevent deleting users who have order?????
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    """
 
 
 class Address(models.Model):
@@ -45,4 +59,6 @@ class Address(models.Model):
     )
 
     # incase of one to many relation between customer and address
+    # the customer can have many address
+    # Address table will have a customer_id foreign key.
     """customer = models.ForeignKey(Customer, on_delete=models.CASCADE)"""
--- tags/models.py
+++ tags/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Order(models.Model):
-    placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(
-        max_length=1,
-        choices=[("P", "Pending"), ("C", "Complete"), ("F", "Failed")],
-        default="P",
-    )
