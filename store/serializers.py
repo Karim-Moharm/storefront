7dd950493ae1002177b6be40884faf7bd3f7e544
karim-moharm <karimmoharm222@gmail.com>
2025-12-11 15:50:38 +0200

    sring relations

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from .models import Product, Collection
+from decimal import Decimal
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
+    # collection_id = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all(), source="collection"
+    # )  # returns id of collection
+
+    Collection_name = serializers.StringRelatedField(source="collection")
+
+    def calc_tax(self, product: Product):
+        return product.price * Decimal(1.1)
