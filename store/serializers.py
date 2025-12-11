1b781de26366c7aa1a7da3ae3ade4a67bd1484fe
karim-moharm <karimmoharm222@gmail.com>
2025-12-11 17:04:01 +0200

    create  and update data

--- store/serializers.py
+++ store/serializers.py
@@ -3,16 +3,30 @@ from .models import Product, Collection
 from decimal import Decimal
 
 
-class ProductSerializer(serializers.Serializer):
+class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
-    # collection_id = serializers.PrimaryKeyRelatedField(
-    #     queryset=Collection.objects.all(), source="collection"
-    # )  # returns id of collection
 
-    Collection_name = serializers.StringRelatedField(source="collection")
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "price",
+            "price_with_tax",
+            "collection",
+            "description",
+            "slug",
+            "inventory",
+        ]
+
+    price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
+
+    # collection = serializers.HyperlinkedRelatedField(
+    #     query_set=Collection.objects.all(), view_name="collection_details"
+    # )
 
     def calc_tax(self, product: Product):
         return product.price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -2,20 +2,45 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 from rest_framework import status
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(
+            data=request.data
+        )  # requets the data in serializer var
+        serializer.is_valid(raise_exception=True)  # validate the data
+        # saving the data in database
+        serializer.save()
+        return Response(
+            f"the object with {serializer.data['id']} was created",
+            status=status.HTTP_201_CREATED,
+        )
+
+
+@api_view(["GET", "PUT"])
+def product_details(request, id):
+    product = get_object_or_404(Product, pk=id)  # object
+    if request.method == "GET":
+        serializer = ProductSerializer(product)  # python dict
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(f"the product {serializer.data['title']} was update")
 
 
 @api_view()
-def product_details(requets, id):
-    product = get_object_or_404(Product, pk=id)  # object
-    serializer = ProductSerializer(product)  # python dict
+def collection_details(request, id):
+    collection = get_object_or_404(Collection, pk=id)
+    serializer = CollectionSerializer(collection)
     return Response(serializer.data)
